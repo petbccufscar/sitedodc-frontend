ec4f1421c87a039c54aab9937f9345b047cacc2e
Daniel Moura <daniel.lucio.moura@gmail.com>
2018-10-03 21:41:10 -0300

    Página dos docentes integrada com o strapi

--- src/routes/Docentes/index.jsx
+++ src/routes/Docentes/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Breadcrumb from "../../components/Breadcrumb";
 import Avatar from "../../images/avatar.png";
+import Docente from "../../components/Docente";
 
 class Docentes extends Component {
   render() {
@@ -10,63 +11,7 @@ class Docentes extends Component {
 
         <h1 class="display-6">Docentes</h1>
         {/* TODO: pegar indo das cards na api Strapi */}
-        <div class="card mb-4">
-          <div class="row no-gutters">
-            <div class="col-auto">
-              <img src={Avatar} class="img-fluid" alt="" />
-            </div>
-            <div class="col ml-1 mt-3">
-              <div class="card-block px-2">
-                <h5 class="card-title">Nome Do Docente Completo</h5>
-                <h6 class="card-subtitle mb-2 text-muted">
-                  Professor Adjunto - 40h/DE
-                </h6>
-                <p class="card-text">
-                  Doutor em Física Computacional - IFSC-USP, Brasil
-                </p>
-                <p class="card-text">
-                  Áreas de interesse: Processamento Digital de Imagens e Sinais,
-                  Reconhecimento de Padrões e Grafos.
-                </p>
-                <a href="#" class="card-link">
-                  Lattes
-                </a>
-                <a href="#" class="card-link">
-                  Página Pessoal
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="card mb-4">
-          <div class="row no-gutters">
-            <div class="col-auto">
-              <img src={Avatar} class="img-fluid" alt="" />
-            </div>
-            <div class="col ml-1 mt-3">
-              <div class="card-block px-2">
-                <h5 class="card-title">Nome Do Docente Completo</h5>
-                <h6 class="card-subtitle mb-2 text-muted">
-                  Professor Adjunto - 40h/DE
-                </h6>
-                <p class="card-text">
-                  Doutor em Física Computacional - IFSC-USP, Brasil
-                </p>
-                <p class="card-text">
-                  Áreas de interesse: Processamento Digital de Imagens e Sinais,
-                  Reconhecimento de Padrões e Grafos.
-                </p>
-                <a href="#" class="card-link">
-                  Lattes
-                </a>
-                <a href="#" class="card-link">
-                  Página Pessoal
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Docente />
       </main>
     );
   }
