7a2999f362e12f433a3c8b58d85386859135893f
João Pedro São Gregorio Silva <pedrosgregorio9@gmail.com>
2018-11-25 12:38:41 -0200

    deixar botao de area mais visivel

--- src/routes/Inicio/components/BotoesArea.jsx
+++ src/routes/Inicio/components/BotoesArea.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const BotaoArea = ({ area = "aluno", texto = "Aluno" }) => (
+const BotaoArea = ({ area = "aluno", texto = "Aluno", className }) => (
   <Link
     to={"/area-" + area}
-    className="btn btn-white py-3 px-md-4  text-primary"
+    className={"btn btn-white py-3 px-md-4  text-primary  " + className }
   >
     <span className="d-none d-sm-inline">Área do </span>{texto}
   </Link>
@@ -12,14 +12,14 @@ const BotaoArea = ({ area = "aluno", texto = "Aluno" }) => (
 const BotoesArea = () => (
   <div className="row justify-content-around ">
     <div
-      className="btn-group col-auto p-0 shadow-sm"
+      className="btn-group col-auto p-0 shadow-sm border"
       role="group"
       id="botoes-area"
       aria-label="First group"
     >
-      <BotaoArea area="aluno" texto="Aluno" />
-      <BotaoArea area="visitante" texto="Visitante" />
-      <BotaoArea area="docente" texto="Docente" />
+      <BotaoArea area="aluno" className="px-4" texto="Aluno" />
+      <BotaoArea area="visitante" className="px-4" texto="Visitante" />
+      <BotaoArea area="docente" className="px-4"  texto="Docente" />
     </div>
   </div>
 );
