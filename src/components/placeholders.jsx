c7e377a7f0f636a995197b856c3add9cc77920bd
Pabolo Pires <pabolo18@gmail.com>
2018-12-14 15:10:54 -0200

    Adiciona placeholders

---
+++ src/components/placeholders.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+export const placeholder = (
+    <React.Fragment>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt
+        sapien vitae lectus semper volutpat. Donec aliquet lectus ut dolor blandit
+        dapibus. Phasellus nulla leo, finibus sit amet orci vel, mollis suscipit
+        sem. Sed pellentesque diam sem, ac malesuada ipsum sollicitudin quis.
+        Vestibulum fermentum lacus quis nunc aliquet commodo. Sed lacinia est non
+        tortor condimentum rutrum. Donec sit amet ex augue. Suspendisse id eros a
+        metus feugiat accumsan. Interdum et malesuada fames ac ante ipsum primis
+        in faucibus. Mauris quis leo sodales, scelerisque dolor quis, vehicula
+        quam. Duis eros tortor, ultricies sed augue vel, malesuada finibus felis.
+        Vestibulum tempor tristique ex eu dictum. Integer finibus eu ligula vitae
+        molestie. Nunc vel velit vitae nulla lobortis congue. Sed dignissim dolor
+        eu leo consequat, vitae cursus orci egestas. Duis ultrices sit amet mauris
+        quis dictum.
+      </p>
+      <p />
+      <p>
+        Aenean sit amet nibh quis enim sodales mollis et id odio. Nulla risus
+        nisi, auctor vitae tortor id, egestas feugiat purus. Etiam egestas pretium
+        molestie. Mauris iaculis dolor quis tortor porttitor tincidunt. Aenean nec
+        est at est rhoncus facilisis ac vel urna. Morbi iaculis purus non risus
+        finibus sodales. Mauris mattis ut ipsum blandit scelerisque. Sed nisi
+        urna, finibus vitae dolor quis, suscipit varius ligula.`;
+      </p>
+      <p />
+    </React.Fragment>
+  );
+  
+export const placeholder2 = (
+    <React.Fragment>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu
+        sapien et quam molestie imperdiet at eget nunc. Morbi sed luctus mauris.
+        Pellentesque tempus maximus maximus. Suspendisse tempor enim id velit
+        congue, rutrum laoreet metus maximus. Donec id accumsan diam, id pharetra
+        ligula. Morbi dui nisl, molestie sit amet dolor ut, sollicitudin porttitor
+        nisi. Aenean viverra augue ac tristique iaculis. Nam egestas nibh mi, at
+        venenatis arcu dictum eu. Nullam et mi eu velit maximus viverra. Praesent
+        dapibus at urna id imperdiet. Curabitur interdum dapibus erat, et
+        ullamcorper turpis congue sed. Vivamus urna sem, suscipit eu feugiat nec,
+        pretium ac dui. Quisque tincidunt quis odio eget ultricies. Etiam
+        tristique ornare turpis eget consequat.
+      </p>
+      <p />
+      <p>
+        Suspendisse sed vestibulum nisl. Nulla nec commodo tellus. Curabitur odio
+        massa, ornare aliquam ligula at, porttitor luctus tellus. Cras egestas
+        sagittis consectetur. Curabitur sollicitudin nisl sed enim sagittis
+        suscipit. Aenean vitae orci nec neque blandit dictum. Proin semper
+        porttitor cursus. Curabitur id diam vitae sapien rutrum pharetra.
+        Phasellus placerat tincidunt nunc ac tempus. Fusce ac libero ante. Donec
+        vulputate tempus leo ac molestie. Mauris venenatis velit nec faucibus
+        feugiat. In et dictum ante, a efficitur nisi.
+      </p>
+      <p />
+      <p>
+        Cras commodo lacinia fermentum. Pellentesque habitant morbi tristique
+        senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+        tincidunt pulvinar ipsum a volutpat. Praesent at mi ac tortor pulvinar
+        dignissim. Mauris vitae mattis nibh. Phasellus volutpat, lorem at blandit
+        mattis, magna turpis laoreet turpis, ac aliquam elit risus et urna. Nam
+        vel tortor nibh. Donec vehicula cursus est a posuere. Nunc metus libero,
+        mattis sodales mauris ac, convallis venenatis orci. Donec sagittis
+        lobortis mauris dictum pellentesque. In ultrices augue elit, in fringilla
+        dui pharetra molestie. Mauris dapibus congue quam sit amet varius. Integer
+        maximus elementum lorem, nec porttitor ex placerat sit amet. Sed id sem
+        vel justo tincidunt interdum.`;
+      </p>
+    </React.Fragment>
+  );
