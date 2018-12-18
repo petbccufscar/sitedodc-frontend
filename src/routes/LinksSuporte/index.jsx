b098200961b7532902892102d2c42af5e66a6c5f
Pabolo Pires <pabolo18@gmail.com>
2018-12-18 16:38:33 -0200

    Fix Bug: Links redirecionando para paginas erradas.

--- src/routes/LinksSuporte/index.jsx
+++ src/routes/LinksSuporte/index.jsx
@@ -3,81 +3,97 @@ import { Breadcrumbs, Breadcrumb } from "../../components/Breadcrumbs";
 import Card, { CardBody } from "../../components/Card";
 import { Link } from "react-router-dom";
 
-
-const Suporte = [
-    {
-        Titulo: "Wi-Fi para visitantes",
-        Link: "http://www.dc.ufscar.br/suporte/nossa-rede/visitante-acesso-wi-fi",
-        Subtitulo: "Guia de acesso Wi-Fi para Visitantes."
-    },
-    {
-        Titulo: "Ambiente de aprendizado (Moodle DC)",
-        Link: "http://moodle.dc.ufscar.br/",
-        Subtitulo: "Moodle do dc."
-    },
-    {
-        Titulo: "Solicitação de afastamento",
-        Link: "http://www3.dc.ufscar.br:8080/Afastamento/",
-        Subtitulo: "Sistema de solicitação de afastamento para professores e funcionários do departamento."
-    },
-    {
-        Titulo: "Troca/Lembrete de senha ",
-        Link: "https://trocasenha.dc.ufscar.br/",
-        Subtitulo: "Sistema para troca e recuperação de senha das contas do domínio dc.ufscar.br."
-    },
-    {
-        Titulo: "Microsoft Imagine",
-        Link: "http://www.dc.ufscar.br/imagine",
-        Subtitulo: "Portal do DC de acesso ao Programa Microsoft Imagine."
-    },
-    {
-        Titulo: "Reserva de Sala",
-        Link: "http://www.dc.ufscar.br/suporte/informacoes-iniciais/reserva-de-salas",
-        Subtitulo: "Orientações e formulário para reserva de sala no DC."
-    },
-    {
-        Titulo: "Solicitação de suporte técnico",
-        Link: "http://suporte.dc.ufscar.br/",
-        Subtitulo: " Link para sistema de solicitação/notificação de suporte técnico."
-    }
+const suporte = [
+  {
+    title: "Ambiente Virtual de Aprendizado (AVA)",
+    subTitle: "",
+    link: "https://ava.ead.ufscar.br/"
+  },
+  {
+    title: "Ambiente de aprendizado (Moodle DC)",
+    subTitle: "Moodle do dc.",
+    link: "http://moodle.dc.ufscar.br/"
+  },
+  {
+    title: "Microsoft Imagine (Indisponível)",
+    subTitle: "Portal do DC de acesso ao Programa Microsoft Imagine.",
+    link: "/imagine"
+  },
+  {
+    title: "Reserva de Sala",
+    subTitle: "Orientações e formulário para reserva de sala no DC.",
+    link: "/suporte/informacoes-iniciais/reserva-de-salas"
+  },
+  {
+    title: "Solicitação de afastamento",
+    subTitle:
+      "Sistema de solicitação de afastamento para professores e funcionários do departamento.",
+    link: "http://www3.dc.ufscar.br:8080/Afastamento/"
+  },
+  {
+    title: "Solicitação de suporte técnico",
+    subTitle:
+      " Link para sistema de solicitação/notificação de suporte técnico.",
+    link: "http://suporte.dc.ufscar.br/"
+  },
+  {
+    title: "Troca/Lembrete de senha ",
+    subTitle:
+      "Sistema para troca e recuperação de senha das contas do domínio dc.ufscar.br.",
+    link: "https://trocasenha.dc.ufscar.br/"
+  },
+  {
+    title: "Wi-Fi para visitantes",
+    subTitle: "Guia de acesso Wi-Fi para Visitantes.",
+    link: "/suporte/nossa-rede/visitante-acesso-wi-fi"
+  }
 ];
 
 class LinksSuporte extends Component {
-    shouldComponentUpdate () {
-        return false;
-      }
-    render() {
-        return (
-            <React.Fragment>
-                <Breadcrumbs>
-                    <Breadcrumb endereco="/">Início</Breadcrumb>
-                    <Breadcrumb >Suporte</Breadcrumb>
+  shouldComponentUpdate() {
+    return false;
+  }
 
-                    <Breadcrumb bold>Links úteis</Breadcrumb>
-                </Breadcrumbs>
-                <div className="container">
-                    <h4>Sistemas</h4>
-                    <div className="card-columns">
-                        {Suporte.map(link => (
-                                <Card className="hoverable"
-                                    key={link.link}
-                                > 
-                                    <CardBody>
-                                        <Link to={link.Link} >
-                                            <h5 className="card-title">{link.Titulo}</h5>
-                                            <p className="card-text">{link.Subtitulo}
-                                            </p>
-                                        </Link>
-                                    </CardBody>
-                                </Card>
-                        )
-                        )}
-                    </div>
+  renderLink(link, title, subTitle) {
+    const content = (
+      <React.Fragment>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{subTitle}</p>
+      </React.Fragment>
+    );
+    const isExternal = !link.startsWith("/");
 
-                </div>
-            </React.Fragment>
-        );
-    }
+    return isExternal ? (
+      <a href={link}>{content}</a>
+    ) : (
+      <Link to={link}>{content}</Link>
+    );
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Breadcrumbs>
+          <Breadcrumb endereco="/">Início</Breadcrumb>
+          <Breadcrumb>Suporte</Breadcrumb>
+
+          <Breadcrumb bold>Links úteis</Breadcrumb>
+        </Breadcrumbs>
+        <div className="container">
+          <h4>Sistemas</h4>
+          <div className="card-columns">
+            {suporte.map(card => (
+              <Card className="hoverable" key={card.link}>
+                <CardBody>
+                  {this.renderLink(card.link, card.title, card.subTitle)}
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default LinksSuporte;
