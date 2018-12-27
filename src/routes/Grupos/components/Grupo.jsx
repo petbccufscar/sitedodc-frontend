e13c8238f7de711fce5eca42e811bc65f9dd3f0d
Pabolo Pires <pabolo18@gmail.com>
2018-12-27 17:33:07 -0200

    Faz as imagens dos grupos redimensionarem em uma proporção de 3:2

--- src/routes/Grupos/components/Grupo.jsx
+++ src/routes/Grupos/components/Grupo.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ImageLoader } from "../../../components/image-loader";
+import { groupImage, imageContainer } from "./style";
 
 // TODO: pegar da api
 const Grupo = ({
   titulo = "Sem titulo",
   descricao = "Sem descrição",
   link,
-  foto,
+  foto
 }) => (
-    <div className="row mt-4  ml-0 mr-0">
-        <div className="col-md-4 mb-2 pl-0 rounded" style={{ minHeight:"200px" ,backgroundImage:'url('+foto+')', backgroundSize:"cover", backgroundPosition:"center"}}>
-    
-        </div>  
-        <div className="col-md-8">
-            <h3>{titulo}</h3>
-            <p className="text-justify">{descricao}</p>
-            <p>
-                <Link to={link}>
-                    <FontAwesomeIcon icon="globe-americas" className="d-inline-block mr-2"/>
-                    Visitar Site
-                </Link>
-            </p>
-        </div>
-</div>
+  <div className="row mt-4 mx-0">
+  <div className="col-md-4 mb-2 pl-0" style={{maxWidth:"100%"}}>
+    <div className={` ${imageContainer}`}>
+      <ImageLoader classnames={`rounded ${groupImage}`} src={foto} />
+    </div ></div>
+    <div className="col-md-8">
+      <h3>{titulo}</h3>
+      <p className="text-justify">{descricao}</p>
+      <p>
+        <Link to={link}>
+          <FontAwesomeIcon
+            icon="globe-americas"
+            className="d-inline-block mr-2"
+          />
+          Visitar Site
+        </Link>
+      </p>
+    </div>
+  </div>
 );
 
 export default Grupo;
