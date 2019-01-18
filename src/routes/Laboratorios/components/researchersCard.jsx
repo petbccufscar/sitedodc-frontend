98c079f56df552d8626b4f7b7afa2180c80ac28f
Caio <caio.b.santos@hotmail.com>
2019-01-18 00:38:55 -0200

    Ajuste nos cards da página de laboratórios, utilizando do component 'card' já criado, e mantendo padrão do site

--- src/routes/Laboratorios/components/researchersCard.jsx
+++ src/routes/Laboratorios/components/researchersCard.jsx
@@ -1,41 +1,45 @@
 import React from "react";
 import ImageLoader from "../../../components/image-loader/ImageLoader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Card, { CardBody, CardFooter } from "../../../components/Card";
 import PropTypes from "prop-types";
-
-import "../../../styles/css/labs.css";
 
 
 const ResearchersCard = (props) => {
     const { nome, funcao, email } = props;
 
     return(
-        <div className="card-body" style={
-            { padding: 0,
-              margin: "10px" }
-            }>
-            <div className="row">
-                <div className="col-md-4" id="imgCard">
-                    <ImageLoader
-                        src={ require("../../../../src/images/imageExample.jpg") }
-                        background-size= "cover"
-                        background-position= "center"
-                        height="100%"
-                        width="100%"
-                        alt="Imagem professor responsável"
-                     />
-                </div>
+       <Card className="hoverable mb-3">
+           <CardBody>
+               <div className="row justify-content-center">
+                    <div className="mr-4 ml-4">
+                        <ImageLoader
+                            classnames="card-img-researchers rounded-circle"
+                            src={ require("../../../../src/images/imageExample.jpg") }
+                            alt="Imagem professor responsável"
+                        />
+                    </div>
+                    <div className="card-content align-self-center" style={{ padding: "1rem" }}>
+                        <h4 className="card-title"> { nome } </h4>
+                        <p className="card-subtitle mb-2 text-muted">{ funcao }</p>
+                        <p className="card-text"><b> Email: </b>{ email } </p>
+                    </div>
+               </div>
+           </CardBody>
+           <CardFooter>
+               <div className="row justify-content-center">
+                    <a href= "#" className="card-link">
+                        <FontAwesomeIcon icon="user" className="align-self-center d-inline-block mr-2"/>
+                        Página Pessoal
+                    </a>
+                    <a href="#" className="card-link">
+                        <FontAwesomeIcon icon="globe-americas" className="align-self-center d-inline-block mr-2"/>
+                        Lattes
+                    </a>
 
-                <div className="col-md-8" style={
-                    { padding: "1.2rem" }
-                }>
-                    <h5 className="card-title"> { nome } </h5>
-                    <p className="card-subtitle mb-2 text-muted">{ funcao }</p>
-                    <p className="card-text"><b> Email: </b>{ email } </p>
-                    <a href= "#" className="card-link">Home Page</a>
-                    <a href="#" className="card-link">Curriculum Lattes</a>
-                </div>
-            </div>
-        </div>
+               </div> 
+           </CardFooter> 
+       </Card>
     );
 };
 
