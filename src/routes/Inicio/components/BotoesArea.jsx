59f15b658781db8b071fd123b2ee4b28a451e4d3
DaanMoura <daniel.lucio.moura@gmail.com>
2019-04-18 13:41:13 -0300

    adicionado pós graduação

--- src/routes/Inicio/components/BotoesArea.jsx
+++ src/routes/Inicio/components/BotoesArea.jsx
@@ -2,16 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Card, { CardBody } from "../../../components/Card";
-// const BotaoArea = ({ to, children }) => (
-//   <Link to={to}>
-//     <button className={"btn btn-white py-3 px-4 text-primary"}>
-//       Área do {children}
-//     </button>
-//   </Link>
-// );
 
 const BotaoArea = ({ to, children }) => (
-  <Card className="mt-2 mb-2 hoverable texte">
+  <Card className="mt-2 mb-2 hoverable">
     <Link to={to}>
       <CardBody>Área do {children}</CardBody>
     </Link>
@@ -26,38 +19,19 @@ BotaoArea.propTypes = {
   ]).isRequired
 };
 
-// const BotoesArea = () => (
-//   <React.Fragment>
-//     <div
-//       className="btn-group-vertical col-auto p-0 shadow-sm border"
-//       // role="group"
-//       // id="botoes-area"
-//       aria-label="First group"
-//     >
-//       <BotaoArea to="/area-aluno" texto="Aluno">
-//         Aluno
-//       </BotaoArea>
-//       <BotaoArea to="/area-visitante" texto="Visitante">
-//         Visitante
-//       </BotaoArea>
-//       <BotaoArea to="/area-docente" texto="Docente">
-//         Docente
-//       </BotaoArea>
-//     </div>
-//   </React.Fragment>
-// );
-
 const BotoesArea = () => (
   <React.Fragment>
-    <BotaoArea to="/area-aluno" texto="Aluno">
-      Aluno
-    </BotaoArea>
-    <BotaoArea to="/area-visitante" texto="Visitante">
-      Visitante
-    </BotaoArea>
-    <BotaoArea to="/area-docente" texto="Docente">
-      Docente
-    </BotaoArea>
+    <div className="d-flex flex-column">
+      <BotaoArea to="/area-aluno" texto="Aluno">
+        Aluno
+      </BotaoArea>
+      <BotaoArea to="/area-visitante" texto="Visitante">
+        Visitante
+      </BotaoArea>
+      <BotaoArea to="/area-docente" texto="Docente">
+        Docente
+      </BotaoArea>
+    </div>
   </React.Fragment>
 );
 
