cbe70d75dd930f300cdcca24fb8e8d2ed77ff7d1
Pabolo Pires <pabolo18@gmail.com>
2019-04-30 15:48:28 -0300

    Alunos são vem do graphql e usam os novos cards

--- src/routes/Alunos/components/style.js
+++ src/routes/Alunos/components/style.js
@@ -1,4 +1,4 @@
-import { css } from 'emotion';
+import { css } from "emotion";
 
 export const row = css`
   display: flex;
@@ -10,8 +10,7 @@ export const column = css`
   margin-bottom: 15px;
   padding: 0 15px;
   justify-content: space-between;
-  
-  
+
   @media (min-width: 576px) {
     width: 50%;
     .card {
@@ -24,7 +23,7 @@ export const column = css`
   @media (min-width: 992px) {
     width: 25%;
   }
-`
+`;
 export const cell = css`
   margin: 0 auto;
   width: 18rem;
@@ -36,4 +35,26 @@ export const cell = css`
   @media (min-width: 576px) {
     width: 14rem;
   }
-`
+`;
+
+export const alunosCard = css`
+  flex: 0 1 calc(100% - 1em);
+  margin: 0 0.5em 1em 0.5em;
+  &:hover {
+    -webkit-box-shadow: 0px 10px 20px 0px rgba(0, 0, 0, 0.2);
+    -moz-box-shadow: 0px 10px 20px 0px rgba(0, 0, 0, 0.2);
+    box-shadow: 0px 10px 20px 0px rgba(0, 0, 0, 0.2); }
+
+  @media (min-width: 576px) {
+    flex: 0 1 calc(50% - 1em);
+    .card {
+      width: 14rem;
+    }
+  }
+  @media (min-width: 768px) {
+    flex: 0 1 calc(33% - 1em);
+  }
+  @media (min-width: 992px) {
+    flex: 0 1 calc(25% - 1em);
+  }
+`;
