a33ba281292d4815b37160fe1cf1d01cf60ddecd
DaanMoura <daniel.lucio.moura@gmail.com>
2019-05-30 14:20:51 -0300

    adicionando o conteudo em Ofertas de Disciplina

--- src/routes/Graduacao/components/Ofertas.jsx
+++ src/routes/Graduacao/components/Ofertas.jsx
@@ -2,204 +2,54 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Ofertas = (
-  <React.Fragment>
-    <div class="accordion" id="accordionExample">
-      <div class="card">
-        <div class="card-header" id="heading-primeiras-disciplinas">
-          <h5 class="mb-0">
-            <button
-              class="btn btn-link"
-              type="button"
-              data-toggle="collapse"
-              data-target="#primeiras-disciplinas"
-              aria-expanded="false"
-              aria-controls="primeiras-disciplinas"
-            >
-              Primeiras Disciplinas
-            </button>
-          </h5>
-        </div>
-
-        <div
-          id="primeiras-disciplinas"
-          class="collapse"
-          aria-labelledby="primeiras-disciplinas"
-          data-parent="#accordionExample"
-        >
-          <div class="card-body">
-            <ul>
-              <li>Introdução a Computação</li>
-              <li>Introdução ao Pensamento Algorítmico</li>
-              <li>Construção de Algorítmos e Programação</li>
-              <li>Lógica Digital</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-header" id="heading-bcc">
-          <h5 class="mb-0">
-            <button
-              class="btn btn-link collapsed"
-              type="button"
-              data-toggle="collapse"
-              data-target="#bcc"
-              aria-expanded="false"
-              aria-controls="bcc"
-            >
-              Bacharelado em Ciência da Computação
-            </button>
-          </h5>
-        </div>
-        <div
-          id="bcc"
-          class="collapse"
-          aria-labelledby="heading-bcc"
-          data-parent="#accordionExample"
-        >
-          <div class="card-body">
-            <p>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-              cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-              laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-              on it squid single-origin coffee nulla assumenda shoreditch et.
-              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-              nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
-              lomo. Leggings occaecat craft beer farm-to-table, raw denim
-              aesthetic synth nesciunt you probably haven't heard of them
-              accusamus labore sustainable VHS.
-            </p>
-            <a href="http://bcc.dc.ufscar.br/">
-              <FontAwesomeIcon
-                icon="globe-americas"
-                className="d-inline-block mr-2"
-              />
-              Visitar Site
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-header" id="heading-enc">
-          <h5 class="mb-0">
-            <button
-              class="btn btn-link collapsed"
-              type="button"
-              data-toggle="collapse"
-              data-target="#enc"
-              aria-expanded="false"
-              aria-controls="enc"
-            >
-              Engenharia da Computação
-            </button>
-          </h5>
-        </div>
-        <div
-          id="enc"
-          class="collapse"
-          aria-labelledby="heading-enc"
-          data-parent="#accordionExample"
-        >
-          <div class="card-body">
-            <p>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-              cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-              laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-              on it squid single-origin coffee nulla assumenda shoreditch et.
-              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-              nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
-              lomo. Leggings occaecat craft beer farm-to-table, raw denim
-              aesthetic synth nesciunt you probably haven't heard of them
-              accusamus labore sustainable VHS.
-            </p>
-            <a href="http://enc.dc.ufscar.br/">
-              <FontAwesomeIcon
-                icon="globe-americas"
-                className="d-inline-block mr-2"
-              />
-              Visitar Site
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-header" id="heading-bsi">
-          <h5 class="mb-0">
-            <button
-              class="btn btn-link collapsed"
-              type="button"
-              data-toggle="collapse"
-              data-target="#bsi"
-              aria-expanded="false"
-              aria-controls="bsi"
-            >
-              Bacharelado em Sistemas de Informação
-            </button>
-          </h5>
-        </div>
-        <div
-          id="bsi"
-          class="collapse"
-          aria-labelledby="heading-bsi"
-          data-parent="#accordionExample"
-        >
-          <div class="card-body">
-            <p>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-              cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-              laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-              on it squid single-origin coffee nulla assumenda shoreditch et.
-              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-              nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
-              lomo. Leggings occaecat craft beer farm-to-table, raw denim
-              aesthetic synth nesciunt you probably haven't heard of them
-              accusamus labore sustainable VHS.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-header" id="heading-outros">
-          <h5 class="mb-0">
-            <button
-              class="btn btn-link collapsed"
-              type="button"
-              data-toggle="collapse"
-              data-target="#outros"
-              aria-expanded="false"
-              aria-controls="outros"
-            >
-              Outros Cursos
-            </button>
-          </h5>
-        </div>
-        <div
-          id="outros"
-          class="collapse"
-          aria-labelledby="heading-outros"
-          data-parent="#accordionExample"
-        >
-          <div class="card-body">
-            <p>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-              cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-              laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-              on it squid single-origin coffee nulla assumenda shoreditch et.
-              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-              nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
-              lomo. Leggings occaecat craft beer farm-to-table, raw denim
-              aesthetic synth nesciunt you probably haven't heard of them
-              accusamus labore sustainable VHS.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </React.Fragment>
+ <React.Fragment>
+   <p>
+       O Departamento de Computação (DC) da Universidade Federal de São Carlos (UFSCar), 
+       campus São Carlos, atua na oferta de disciplinas de graduação que abrangem conhecimentos 
+       em diferentes áreas da computação, sendo elas: Teoria da Computação, Matemática da Computação, 
+       Metodologia e Técnicas da Computação, e Sistemas de Computação. As disciplinas cobrem 
+       aspectos teóricos e práticos e são ministradas tanto nos laboratórios do DC como em outros espaços na UFSCar.
+   </p>
+   <p>
+       Majoritariamente, nossas ofertas de disciplinas são para os cursos de Bacharelado em 
+       Ciência da Computação (BCC) e Engenharia da Computação (EnC). No entanto, também há 
+       oferta de disciplinas para outros cursos como Engenharias, Estatística e Matemática.
+   </p>
+   <p>
+       Para o curso de Bacharelado em Ciência da Computação, o DC oferece disciplinas de base 
+       e também aquelas de formação específica. O curso está dividido em sete núcleos de conhecimento: 
+       Fundamentos de Matemática e Estatística, Fundamentos de Ciência da Computação, Algoritmos e 
+       Programação, Metodologia e Técnicas da Computação, Sistemas de Computação, Formação 
+       Multidisciplinar e Humanística e Orientações. O <a target="blank"
+         href="http://www.prograd.ufscar.br/cursos/cursos-oferecidos-1/ciencia-da-computacao/PPCBCC_2019.pdf">
+         Projeto Pedagógico do BCC 
+       </a> apresenta, entre outras, as disciplinas oferecidas pelo DC aos alunos desse curso. 
+       Para outras informações, visite o <a target="blank"
+        href="http://bcc.dc.ufscar.br/">
+         site do curso</a>.
+   </p>
+   <p>
+       No curso de Engenharia de Computação, o DC também oferece disciplinas de base e aquelas 
+       de conhecimento específico. O conteúdo do curso está distribuído em cinco núcleos básicos 
+       e tecnológicos: Fundamentos de Matemática e Estatística, Algoritmos e Programação, Arquiteturas 
+       de Computadores, Metodologia e Técnicas da Computação, e Humanas. O <a target="blank"
+         href="http://www.prograd.ufscar.br/cursos/cursos-oferecidos-1/engenharia-de-computacao/PPC_EC_2019.pdf">
+         Projeto Pedagógico da EnC </a> apresenta, entre outras, as disciplinas oferecidas pelo 
+       DC aos alunos desse curso. 
+       Para outras informações, visite o <a target="blank"
+        href="http://enc.dc.ufscar.br/">
+         site do curso</a>.
+   </p>
+   <p>
+       Desde 2007, o DC oferece disciplinas na modalidade à distância para alunos do curso de 
+       Bacharelado em Sistemas de Informação, como parte do projeto Universidade Aberta do 
+       Brasil – UFSCar. O Bacharelado em Sistemas de Informação tem a computação como atividade meio, 
+       pois reúne as tecnologias das áreas de Computação, de Sistemas de Informação e de Administração. 
+       Para outras informações, visite o <a target="blank"
+        href="http://www.sead.ufscar.br/cursos/graduacao/sistemas-de-informacao/">
+         site do curso</a>.
+   </p>
+ </React.Fragment>
 );
 
 export default Ofertas;
