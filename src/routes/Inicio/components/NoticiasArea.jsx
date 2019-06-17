752205be1ca44f763562ba3c9ac0dc268d9b0e7b
Pabolo Pires <pabolo18@gmail.com>
2019-06-17 03:21:11 -0300

    Alinha noticias e barra lateral

--- src/routes/Inicio/components/NoticiasArea.jsx
+++ src/routes/Inicio/components/NoticiasArea.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NoticiasArea = ({ children }) => {
-  return <div className="col-12 col-md-8">{children}</div>;
+  return <div className="col-12 col-md-8 d-flex flex-column">{children}</div>;
 };
 
 export default NoticiasArea;
