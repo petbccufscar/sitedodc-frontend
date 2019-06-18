b4ca1648c121f2fb9f3abefc36445bffda7ca0b5
DaanMoura <daniel.lucio.moura@gmail.com>
2019-06-18 08:26:42 -0300

    arrumando alinhamento dos cards de notícias

--- src/routes/Inicio/components/NoticiasArea.jsx
+++ src/routes/Inicio/components/NoticiasArea.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NoticiasArea = ({ children }) => {
-  return <div className="col-12 col-md-8 d-flex flex-column">{children}</div>;
+  return <div className="col-12 col-md-8 ">{children}</div>;
 };
 
 export default NoticiasArea;
