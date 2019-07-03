eccd02c6007434da4a58780d3f7c5bbdba244a54
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2019-07-02 22:11:50 -0300

    alterando planilha de distrib

--- src/routes/AreaAluno/components/Distribuicao.jsx
+++ src/routes/AreaAluno/components/Distribuicao.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Distribuicao = (
   <React.Fragment>
     <div className="d-lg-none">
-        <a target="blank" href="https://drive.google.com/file/d/1pQHKCEgqD20WhEbNwhT8yTmtcHiE6rsr/view"><p>Distribuição das disciplinas para 2019.2</p></a>
+        <a target="blank" href="distrib_grad.pdf"><p>Distribuição das disciplinas para 2019.2</p></a>
     </div>
     <div className="d-none d-lg-block">
         <embed src="/distrib_grad.pdf" type="application/pdf" width="100%" height="600px"></embed>
