d9a6bb7ccc68fba8f6667cb3c8c37869b4d55bb9
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2019-07-24 18:11:29 -0300

    Alteração que faltou

--- src/routes/AreaAluno/components/Distribuicao_pos.jsx
+++ src/routes/AreaAluno/components/Distribuicao_pos.jsx
@@ -6,7 +6,7 @@ const Distribuicao_pos = (
       <a target="blank" href="distrib_pos.pdf"><p>Distribuição das disciplinas para 2019.2</p></a>
     </div>
     <div className="d-none d-lg-block">
-        <p>Distribuição das disciplinas para 2019.2</p>
+        <center><p>Distribuição das disciplinas para 2019.2</p></center>
         <embed src="/distrib_pos.pdf" type="application/pdf" width="100%" height="600px"></embed>
     </div>
   </React.Fragment>
