ad9795b66669b5713be638995a92130e42b84bf4
DaanMoura <daniel.lucio.moura@gmail.com>
2019-07-23 22:47:13 -0300

    atualizando textos

--- src/routes/AreaVisitante/components/Motivacao.jsx
+++ src/routes/AreaVisitante/components/Motivacao.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Motivacao = (
   <React.Fragment>
     <p>
-      A UFSCar foi classificada na 10ª posição entre as melhores universidades do Brasil pelo Ranking Universitário Folha (RUF) em 2017. O RUF é uma avaliação anual do Ensino Superior do País feita pelo jornal Folha de S. Paulo desde 2012. Também segundo avaliação do Ministério da Educação (MEC) divulgada em novembro de 2017, a Universidade atingiu a nota máxima (5) no ICG (Índice Geral de Cursos), indicador de qualidade do Ensino Superior, e está entre as 10 melhores universidades do País. Já o Times Higher Education (THE), um dos principais rankings internacionais de universidades, colocou a UFSCar em 18º lugar entre 81 instituições de Ensino Superior da América Latina.
+    A UFSCar foi classificada na 10ª posição entre as melhores universidades do Brasil pelo Ranking Universitário Folha (RUF) em 2017. O RUF é uma avaliação anual do Ensino Superior do País feita pelo jornal Folha de S. Paulo desde 2012. Também segundo avaliação do Ministério da Educação (MEC) divulgada em novembro de 2017, a Universidade atingiu a nota máxima (5) no ICG (Índice Geral de Cursos), indicador de qualidade do Ensino Superior, e está entre as 10 melhores universidades do País. Já o Times Higher Education (THE), um dos principais rankings internacionais de universidades, colocou a UFSCar em 16º lugar em 2019, dentre as 150 universidades latino-americanas avaliadas. Considerando apenas as 52 universidades brasileiras que entraram na lista do THE, a UFSCar ocupa a 11ª posição.
    </p>
   </React.Fragment>
 );
