08c561f60e2466294225bf4837bf0b7f7874936e
DaanMoura <daniel.lucio.moura@gmail.com>
2020-03-03 19:03:21 -0300

    documentos de estágio atualizados

--- src/routes/AreaAluno/components/Estagio.jsx
+++ src/routes/AreaAluno/components/Estagio.jsx
@@ -1,27 +1,68 @@
 import React from "react";
 
 const Estagio = (
-  <React.Fragment>
-    <p>O Estágio é uma atividade curricular obrigatória realizada no final do curso visando preparar o estudante para o exercício profissional. As atividades de estágio são orientadas por um docente do curso por meio de relatórios técnicos e acompanhamento individualizado durante o período de realização do mesmo e supervisionadas por um profissional do local onde se realiza o estágio. O Regulamento do Estágio Curricular Obrigatório e Não-obrigatório pode ser consultado nos Projetos Pedagógicos dos cursos.</p>
-    <p>Consulte também outros <a href="https://drive.google.com/file/d/1dy1D1hyZa-lp15J7YbQ1IfgG0YYp4oga/view" target="blank">esclarecimentos sobre a realização de estágio sob a orientação de professores do DC.</a></p>
+	<React.Fragment>
+		<p>
+			O Estágio é uma atividade curricular obrigatória realizada no final do curso visando preparar o estudante
+			para o exercício profissional. As atividades de estágio são orientadas por um docente do curso por meio de
+			relatórios técnicos e acompanhamento individualizado durante o período de realização do mesmo e
+			supervisionadas por um profissional do local onde se realiza o estágio. O Regulamento do Estágio Curricular
+			Obrigatório e Não-obrigatório pode ser consultado nos Projetos Pedagógicos dos cursos.
+		</p>
+		<p>
+			Consulte também outros{' '}
+			<a href="https://drive.google.com/file/d/1dy1D1hyZa-lp15J7YbQ1IfgG0YYp4oga/view" target="blank">
+				esclarecimentos sobre a realização de estágio sob a orientação de professores do DC.
+			</a>
+		</p>
 
-    <ul>
-      <li><a href="/estagio/Lei_do_Estagio.pdf" target="blank">Lei do Estágio</a></li>
-      <li><a href="/estagio/Ministerio_do_Trabalho_Nova_Cartilha_do_Estagio.pdf
-" target="blank">Nova Cartilha do Estágio</a></li>
-    </ul>
+		<ul>
+			<li>
+				<a href="/estagio/Lei_do_Estagio.pdf" target="blank">
+					Lei do Estágio
+				</a>
+			</li>
+			<li>
+				<a
+					href="/estagio/Ministerio_do_Trabalho_Nova_Cartilha_do_Estagio.pdf
+"
+					target="blank"
+				>
+					Nova Cartilha do Estágio
+				</a>
+			</li>
+		</ul>
 
-    <ul>
-      <li><a href="/estagio/TermoCompromisso_Est_NaoObrig_BCC-Modelo.docx" target="blank">BCC - Estágio não obrigatório - Termo de Compromisso</a></li>
-      <li><a href="/estagio/TermoCompromisso_Est_Obrig_BCC.docx" target="blank">BCC - Estágio obrigatório - Termo de Compromisso</a></li>
-      <li><a href="/estagio/Aditivo-mudanca_NaoObrigatorio-Obrigatorio_BCC-CH.docx" target="blank">BCC - Mudança de estágio não obrigatório para obrigatório - Aditivo</a></li>
-    </ul>
-
-    <ul>
-      <li><a href="/estagio/Aditivo-mudanca_NaoObrigatorio-Obrigatorio_EnC.docx" target="blank">ENC - Mudança de estágio não obrigatório para obrigatório - Aditivo</a></li>
-      <li><a href="/estagio/Aditivo-mudanca_Obrigatorio-NaoObrigatorio_EnC.docx" target="blank">ENC - Mudança de estágio obrigatório para não obrigatório - Aditivo</a></li>
-    </ul>
-  </React.Fragment>
+		<ul>
+			<li>
+				<a href="/estagio/Termo_Compr_Estagio_NaoObrigatorio.docx" target="blank">
+					Estágio não-obrigatório - Termo de Compromisso
+				</a>
+			</li>
+			<li>
+				<a href="/estagio/Termo_Compr_Estagio_Obrigatorio.docx" target="blank">
+					Estágio obrigatório - Termo de Compromisso
+				</a>
+			</li>
+			<li>
+				<a href="/estagio/Aditivo_TCE.docx" target="blank">
+					Termo Aditivo ao Compromisso de Estágio
+				</a>
+				<ul>
+					<li>
+						<a href="/estagio/Anexo1_Aditivo_TCE.docx" target="blank">
+							Anexo 1
+						</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="/estagio/Recisao_TCE.docx" target="blank">
+					Termo de Rescisão de Estágio
+				</a>
+			</li>
+		</ul>
+	</React.Fragment>
 );
 
 export default Estagio;
