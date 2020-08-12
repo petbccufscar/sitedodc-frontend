aec4b963395c8226686a48c2d2445d1c493d6720
Your Name <donderileo@gmail.com>
2020-08-12 16:04:26 -0300

    Distribuição 2020 - Remoto

--- src/routes/AreaAluno/components/Distribuicao.jsx
+++ src/routes/AreaAluno/components/Distribuicao.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Distribuicao = (
   <React.Fragment>
     <div className="d-lg-none">
-        <a target="blank" href="distrib_grad.pdf"><p>Distribuição das disciplinas para 2019.2</p></a>
+        <a target="blank" href="distrib_grad.pdf"><p>Distribuição das disciplinas para 2020 - Remoto</p></a>
     </div>
     <div className="d-none d-lg-block">
-        <center><p>Distribuição das disciplinas para 2019.2</p></center>
+        <center><p>Distribuição das disciplinas para 2020 - Remoto</p></center>
         <embed src="/distrib_grad.pdf" type="application/pdf" width="100%" height="600px"></embed>
-        <a target="blank" href="distrib_grad.pdf"><p>Distribuição das disciplinas para 2019.2</p></a>
+        <a target="blank" href="distrib_grad.pdf"><p>Distribuição das disciplinas para 2020 - Remoto</p></a>
 
     </div>
   </React.Fragment>
